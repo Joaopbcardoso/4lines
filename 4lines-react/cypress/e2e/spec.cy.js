91f05702ad0896217b8b0fc5d869b2266284b3e0
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2024-10-24 10:38:49 -0300

    userType

--- 4lines-react/cypress/e2e/spec.cy.js
+++ 4lines-react/cypress/e2e/spec.cy.js
@@ -44,6 +44,50 @@ describe('Testes de Cadastro e Login', () => {
 
   });
 
+  context('Login para Usuário Comum', () => {
+    
+    it('Deve carregar a página de Login para Usuário Comum', () => {
+      cy.visit('/');  
+      cy.get('form').within(() => {
+        cy.get('input[name="user"]').should('be.visible');
+        cy.get('input[name="senha"]').should('be.visible');
+      });
+    });
+
+    it('Deve fazer login de usuário comum com sucesso', () => {
+      cy.visit('/');  
+      cy.get('input[name="user"]').type('userTeste');
+      cy.get('input[name="senha"]').type('user123');
+      cy.get('input[type="submit"]').click();
+      cy.url().should('include', '/Home');  
+    });
+
+  });
+
+  context('Cadastro para Usuário Comum', () => {
+
+    it('Deve carregar a página de Cadastro de Usuário Comum', () => {
+      cy.visit('/Cadastro'); 
+      cy.get('form').within(() => {
+        cy.get('input[name="user"]').should('be.visible');
+        cy.get('input[name="email"]').should('be.visible');
+        cy.get('input[name="senha"]').should('be.visible');
+        cy.get('input[name="senha2"]').should('be.visible');
+      });
+    });
+
+    it('Deve submeter o formulário de Cadastro de Usuário Comum corretamente', () => {
+      cy.visit('/Cadastro');  
+      cy.get('input[name="user"]').type('userTeste');
+      cy.get('input[name="email"]').type('user@teste.com');
+      cy.get('input[name="senha"]').type('user123');
+      cy.get('input[name="senha2"]').type('user123');
+      cy.get('input[type="submit"]').click();
+      cy.url().should('include', '/'); 
+    });
+
+  });
+
   context('Home', () => {
   
     it('Deve carregar a página Home após login do usuário comum', () => {
